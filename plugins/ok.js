2e71526625f878f669c2884e6c27779306871f68
rodriku <script101a4@gmail.com>
2022-05-03 08:28:52 -0500

    version6

--- plugins/ok.js
+++ plugins/ok.js
@@ -8,6 +8,6 @@ type: 'audioMessage',
 ptt: true 
 })
 }
-handler.customPrefix = /ok|OK|ok mi amor|ya|Ya|yes|yes|Yess|ven|Ven|si|Si|siii|no|No|nooo|noo|Noo|no demores|No demores|si primo|Si primo|si hijo|Si hijo|Yes|ok|Ok bro|ok primo|ok vale|Ok vale|gracias|Ok|si|gracias amigo|graciass|Gracias bro|gracias bro|Ok|gracias sobrino|gracias sobrina|Gracias sobrino|gracias amigo|Gracias amigo|Gracias mano|gracias mano|ok bro|ok/
+handler.customPrefix = /ok|OK|ok mi amor|ya|Ya|ven|Ven|si|Si|siii|no|No|nooo|noo|Noo|si primo|Si primo|si hijo|Si hijo|Yes|ok|Ok bro|ok primo|ok vale|Ok vale|gracias|Ok|si|gracias amigo|graciass|Gracias bro|gracias bro|Ok|gracias|Gracias|gracias amigo|Gracias amigo|Gracias mano|gracias mano|ok bro|ok/
 handler.command = new RegExp
 module.exports = handler
